38965c8993e46544798e4a0f96beeb958ad2fa43
Anthony Olivera <78010942+Banquoslayer@users.noreply.github.com>
2022-09-15 00:24:31 -0400

    Add files via upload

---
+++ rtweekend.h
@@ -0,0 +1,28 @@
+#ifndef RTWEEKEND_H
+#define RTWEEKEND_H
+
+#include <cmath>
+#include <limits>
+#include <memory>
+
+using std::shared_ptr;
+using std::make_shared;
+using std::sqrt;
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
+}
+
+#include "ray.h"
+#include "vec3.h"
+
+#endif
